bf3daafe350fa122d806876be5c4193988fd4a33
ChalakaK94 <karunarathne.cp@gmail.com>
2023-12-08 21:10:43 +0530

    implement side nav and footer
    Signed-off-by: ChalakaK94 <karunarathne.cp@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 
 import './App.css';
 import {Navbar} from "./components/Navbar";
-import {Layout, } from "antd";
-import {Switch, Route, Link, Routes} from 'react-router-dom';
+import {Layout, Space, Typography,} from "antd";
+import { Route, Link, Routes} from 'react-router-dom';
+import {Home} from "./components/Home";
+import {Exchanges} from "./components/Exchanges";
+import {Cryptocurrencies} from "./components/Cryptocurrencies";
+import {CryptoDetails} from "./components/CryptoDetails";
+import {News} from "./components/News";
+
 
 function App() {
   return (
@@ -13,25 +19,26 @@ function App() {
       <div className='main'>
         <Layout>
             <div className='routes'>
-                <Switch>
-                    <Routes exact  path='/'>
-                        <HomePage/>
-                    </Routes>
-                    <Routes exact  path='/exchanges'>
-                        <Exchanges/>
-                    </Routes>
-                    <Routes exact  path='/cryptocurrencies'>
-                        <Cryptocurrencies/>
-                    </Routes>
-                    <Routes exact  path='/cryptocurrencies'>
-                        <Cryptocurrencies/>
-                    </Routes>
-                </Switch>
+                <Routes>
+                    <Route exact path="/" element={<Home/>}/>
+                    <Route   path='/exchanges' element={<Exchanges/>}/>
+                    <Route   path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+                    <Route   path='/crypto:coinId' element={<CryptoDetails/>}/>
+                    <Route   path='/news' element={<News/>}/>
+                </Routes>
             </div>
         </Layout>
-      </div>
-        <div className='footer'>
-
+            <div className='footer'>
+                <Typography.Title  level={5} style={{color:'white', textAlign:'center'}}>
+                    CryptoArea <br/>
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to='/'>Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
+          </div>
       </div>
     </div>
   );
---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,5 @@
+export function CryptoDetails() {
+    return (
+        <></>
+    )
+}
