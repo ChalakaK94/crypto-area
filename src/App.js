6781a367092c8901d75a301f9eb9d969043b0219
ChalakaK94 <karunarathne.cp@gmail.com>
2023-12-08 18:52:04 +0530

    implement header component and side nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Navbar} from "./components/Navbar";
+import {Layout, } from "antd";
+import {Switch, Route, Link, Routes} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className='app'>
+      <div className='navbar'>
+          <Navbar/>
+      </div>
+      <div className='main'>
+        <Layout>
+            <div className='routes'>
+                <Switch>
+                    <Routes exact  path='/'>
+                        <HomePage/>
+                    </Routes>
+                    <Routes exact  path='/exchanges'>
+                        <Exchanges/>
+                    </Routes>
+                    <Routes exact  path='/cryptocurrencies'>
+                        <Cryptocurrencies/>
+                    </Routes>
+                    <Routes exact  path='/cryptocurrencies'>
+                        <Cryptocurrencies/>
+                    </Routes>
+                </Switch>
+            </div>
+        </Layout>
+      </div>
+        <div className='footer'>
 
+      </div>
     </div>
   );
 }
